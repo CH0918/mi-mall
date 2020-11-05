8fd891c30d634e70cf753845ed702e09d494ca2e
huangdongjiang <dongjiang@adbright.cn>
2020-11-05 08:38:47 +0800

    路由封装

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,13 @@
 <template>
   <div id="app">
-    {{ name }}
     <router-view />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
 export default {
-  data() {
-    return {
-      name: ''
-    }
-  },
-  mounted() {
-    const url = '/data.json'
-    axios.get(url).then(res => {
-      this.name = res.data.name
-    })
-  }
+  data() {},
+  mounted() {}
 }
 </script>
 <style></style>
--- src/components/OrderHeader.vue
+++ src/components/OrderHeader.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    detail11
-  </div>
+  <div>order header</div>
 </template>
 <script>
 export default {
-  name: 'detail'
+  name: 'order-header'
 }
 </script>
